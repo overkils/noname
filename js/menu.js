49c8cd013cb3d7c6a7e9cc3cebff317dd286ebe2
overkils <tarasevich.daniils@gmail.com>
2022-01-08 14:41:47 +0300

    еще кое какие исправления в меню...

--- js/menu.js
+++ js/menu.js
@@ -3,29 +3,41 @@ var menu_btn = _('.header__menu-btn'); // поиск кнопки меню в he
 var bot_btn = _('.bot__menu-btn'); // поиск кнопки закрытия в нижнем меню
 var menu_btn_close = _('.bot__menu-btns'); // поиск кнопки закрытия в под меню
 var header_menu = _('.header__menu');
+var header_bot = _('.header__bot');
 
 document.querySelector('.header__bot').addEventListener('click', e => {
     //проверка наличия bot__link в bot__item
     if (!e.target.classList.contains('bot__btn')) return;
 
     function myFunction(x) {
-        if (x.matches) { // If media query matches
-            header_menu.style.display = 'none'; // отключаем меню до его открытия
-            data_pages = e.target.getAttribute('data-page'); // заносим в глобал переменную номер страницы
-            toggleBlock(); // выдаем меню block для того чтобы показать его
-            menuActive(); //выдача класса активности(открытого) меню
-            setTimeout(menuActiveS, 100) // отображение меню на адаптив версии 
-            replacePage(); // замена номера страницы в функции 
-            _('.bot__nav-title').textContent = e.target.textContent; // переносим название кнопки в тайтл меню
-            e.target.toggleAttribute('activitis'); // нажатой кнопке выдаем атрибут активности
-            e.target.classList.toggle('bot__btn-active'); // а тае же класс активности для стилей 
+        if (x.matches) { // If media query matches // а тае же класс активности для стилей 
+            if (header_bot.classList.contains('active')) {
+                header_menu.style.display = 'none'; // отключаем меню до его открытия
+                data_pages = e.target.getAttribute('data-page'); // заносим в глобал переменную номер страницы
+                console.log(data_pages)
+                toggleBlock(); // выдаем меню block для того чтобы показать его
+                menuActive(); //выдача класса активности(открытого) меню
+                setTimeout(menuActiveS, 100) // отображение меню на адаптив версии 
+                replacePage(); // замена номера страницы в функции 
+                _('.bot__nav-title').textContent = e.target.textContent; // переносим название кнопки в тайтл меню
+                e.target.toggleAttribute('activitis'); // нажатой кнопке выдаем атрибут активности
+                e.target.classList.toggle('bot__btn-active');
+            }
         } else {
-            data_pages = e.target.getAttribute('data-page'); // заносим в глобал переменную номер страницы
-            replacePage(); // замена номера страницы в функции 
-            e.target.toggleAttribute('activitis'); // нажатой кнопке выдаем атрибут активности
-            e.target.classList.toggle('bot__btn-active'); // а тае же класс активности для стилей 
+            if (header_menu.getAttribute('data-page') === null || data_pages === undefined) {
+                data_pages = e.target.getAttribute('data-page'); // заносим в глобал переменную номер страницы
+                replacePage(); // замена номера страницы в функции 
+                e.target.toggleAttribute('activitis'); // нажатой кнопке выдаем атрибут активности
+                e.target.classList.toggle('bot__btn-active'); // а тае же класс активности для стилей 
+            } else if (data_pages != undefined) {
+                data_pages = e.target.getAttribute('data-page'); // заносим в глобал переменную номер страницы
+                replacePage(); // замена номера страницы в функции 
+                e.target.toggleAttribute('activitis'); // нажатой кнопке выдаем атрибут активности
+                e.target.classList.toggle('bot__btn-active'); // а тае же класс активности для стилей 
+            }
         }
     }
+
     var x = window.matchMedia("(max-width: 992px)")
     myFunction(x) // Call listener function at run time
     x.addEventListener('change', myFunction) // Attach listener 
@@ -40,6 +52,7 @@ document.querySelector('.header__bot').addEventListener('click', e => {
     //отключение меню при отсутствии активной кнопки 
     var closet_menu = document.querySelector('[activitis]'); // ищем атрибут активности на кнопках
     if (closet_menu === null) { // если его нету то
+        data_pages = undefined;
         deletePageAttr(); // убираем атрибут номера страницы и 
         setTimeout(removeActives, 800);
         function myFunction(x) {
@@ -47,9 +60,6 @@ document.querySelector('.header__bot').addEventListener('click', e => {
                 setTimeout(nonedisplayMenu, 400); // отключаем меню для того чтоб не захломлять не поточную позиционированность
             }
         }
-        var x = window.matchMedia("(max-width: 700px)") // изменяем значение на то которое нужно для того чтоб скрипты работали
-        myFunction(x) // Call listener function at run time
-        x.addEventListener('change', myFunction) // Attach listener
     };
 });
 
@@ -80,14 +90,13 @@ function replacePage() {
 
 function visibleBtn() {
     _('.header__bot').classList.toggle('active'); //выдаем класс активности для открытия меню с выбором категории
-    setTimeout(() => {
-        _('.header__bot').classList.toggle('actives');
-    }, 100);
     menu_btn.classList.toggle('active'); // выдача класса активности меню что бы открыть меню и изменить стили
     removeActives();
 };
 
 function closeMenuArrow() {
+    data_pages = undefined;
+    console.log(data_pages);
     var closet_menu = document.querySelector('[activitis]'); // ищем атрибут активности на кнопках
     closet_menu.removeAttribute('activitis'); // удаляем атрибут активности с кнопок
     closet_menu.classList.toggle('bot__btn-active'); // убираем класс активности с кнопок
@@ -101,7 +110,16 @@ function closeMenuArrow() {
 };
 
 function nonedisplayMenu() {
-    header_menu.style.display = "none";
+    function myFunction(x) {
+        if (x.matches) { // а тут указываем то что должно работать после достижения какого либо брейкпоинта
+            header_menu.style.display = "none";
+        } else {
+            header_menu.style.display = "block";
+        }
+    }
+    var x = window.matchMedia("(max-width: 992px)")
+    myFunction(x) // Call listener function at run time
+    x.addEventListener('change', myFunction) // Attach listener 
 };
 
 function displayMenu() {
